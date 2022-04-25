ecd03e41e93ed03f71c282fdc7ed9d73be0d0119
AngieBinnsM <binns.am.1@pg.com>
2022-04-25 15:17:32 -0600

    frontend size

--- frontend/src/style.js
+++ frontend/src/style.js
@@ -3,6 +3,6 @@ import bluePaint from "./blue-abstract.png";
 export const body = {
   backgroundImage: `url(${bluePaint})`,
   backgroundRepeat: "no-repeat",
-  backgroundPosition: "-190px -338px",
+  backgroundPosition: "-169px -354px",
   backgroundAttachment: "fixed",
 };
